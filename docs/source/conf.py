fd1769835d07775afcbd7569afa871518a190eb8
Jeff Scheel <jeff@riscv.org>
2024-02-19 09:50:04 -0500

    Fav icon, theme updates
    Signed-off-by: Jeff Scheel <jeff@riscv.org>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,8 +34,37 @@ intersphinx_disabled_domains = ['std']
 templates_path = ['_templates']
 
 # -- Options for HTML output
+github_url = 'https://github.com/riscv-admin/template-docs-rtd'
+html_sho_sourcelink = True
 
 html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
+html_css_files = ['css/risc-v.css']
+html_style = 'css/risc-v.css'
+
+html_logo = 'RISC-V-Logo.svg'
+
+html_theme_options = {
+#   'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+#   'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': False,
+    'titles_only': False
+}
+
+html_show_license = True
+
+html_favicon = '_static/riscv-favicon-32x32.png'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
